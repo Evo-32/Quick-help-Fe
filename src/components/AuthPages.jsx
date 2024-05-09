ba1192281a1fe0e63663a46002d274197d5cffb7
Naomii01 <naomi222001220@gmail.com>
2024-05-09 09:23:29 +0200

    changes

---
+++ src/components/AuthPages.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+
+const AuthPages = () => {
+  return (
+    <>
+        <Outlet />
+    </>
+  )
+}
+
+export default AuthPages
---
+++ src/components/Content.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../styles/content.css"
+import ContentHeader from './ContentHeader'
+
+const Content = () => {
+  return (
+    <div className='content'>
+       <ContentHeader/>
+       </div>
+  )
+}
+
+export default Content
